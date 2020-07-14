d84844b1bf85a1224bd961a0dd724cb392207713
bandera2014 <nicholas.bander@evsck12.com>
2020-07-14 09:34:02 -0500

    Reverse commented

--- PR-2/ReverseGuessing.py
+++ PR-2/ReverseGuessing.py
@@ -5,11 +5,14 @@ import math
 print ("Welcome to the Reverse Guessing Game!\n")
 
 # computer picks a number between 1 and 1000
+#selection
 numberOfAttempts = int(input("How many Games would you like to play? "))
 
 #highLow will be user input to state h,l,e
+#variables
 highLow=""
 
+#event
 for i in range(numberOfAttempts):
   tries=0
   #target value for computer to hit
@@ -17,9 +20,10 @@ for i in range(numberOfAttempts):
   min,max=1,1000
   guess = random.randint(min,max)
   print("My guess is {}".format(guess))
-  #guessing loop
+  #guessing loop or iteration
   while True:
     highLow=input("Is my guess to (h)high , too (l)low, or (e)equal to your number? ")
+    #conditional
     if highLow=="h":
       max=guess 
     elif highLow=="l":
@@ -30,4 +34,20 @@ for i in range(numberOfAttempts):
     guess = int((min+max)/2)
     print("My guess is {}".format(guess))
     tries+=1
-    
+
+'''
+Difficulty in writing the assignment - Ususally I have to guess the number, so it
+  is strange thinking of how to make the computer guess which number I had.
+
+Easiest in writing the assignmnet - Luckily the flow followed the original
+  version of the guessing game, so it was easy to start
+
+Changes for next time - It would be nice to have the computer guess randomly or
+  using the average.  If I were to assign this game, I would have my students
+  add in this feature, and if we get to lists, have the computer store the values
+  in memory and use that somehow to guess.
+
+'''
+
+
+
