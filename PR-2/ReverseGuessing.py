64ba032bdfb9b2bcdc3bc88532e658ee47d57de0
bandera2014 <nicholas.bander@evsck12.com>
2020-07-14 06:37:57 -0500

    finished ReverseGuessing

--- PR-2/ReverseGuessing.py
+++ PR-2/ReverseGuessing.py
@@ -7,30 +7,27 @@ print ("Welcome to the Reverse Guessing Game!\n")
 # computer picks a number between 1 and 1000
 numberOfAttempts = int(input("How many Games would you like to play? "))
 
+#highLow will be user input to state h,l,e
 highLow=""
-
-
 
 for i in range(numberOfAttempts):
   tries=0
-  target = int(input('''Choose a number between 1 and 100 inclusive, and I (the computer) will try to guess it. '''))
-  min,max=1,100
+  #target value for computer to hit
+  target = int(input("Choose a number between 1 and 1000 inclusive, and I (the computer) will try to guess it. "))
+  min,max=1,1000
   guess = random.randint(min,max)
   print("My guess is {}".format(guess))
-  correct=False
-  while correct!=True:
+  #guessing loop
+  while True:
     highLow=input("Is my guess to (h)high , too (l)low, or (e)equal to your number? ")
     if highLow=="h":
-      print('elif high')
-      max=guess
-      
+      max=guess 
     elif highLow=="l":
-      print('elif low')
       min=guess
-      
     elif highLow=="e":
       print("I win!  It took me {} guess(es)".format(tries))
-      correct=True
+      break
     guess = int((min+max)/2)
+    print("My guess is {}".format(guess))
     tries+=1
     
